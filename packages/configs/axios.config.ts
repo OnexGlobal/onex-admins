4a34310273646c493a3ff3b1832842863163387e
Artashes <developer.artashes@gmail.com>
2024-11-14 04:53:26 -0800

    fixes

--- packages/configs/axios.config.ts
+++ packages/configs/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const token = localStorage.getItem("token");
-axios.defaults.baseURL = "https://stagingbackadmin.onex.ge/api";
+axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL_DEV;
 axios.defaults.headers.common = {
   ["Authorization"]: token ? `Bearer ${token}` : undefined,
   "X-localization": "en",
