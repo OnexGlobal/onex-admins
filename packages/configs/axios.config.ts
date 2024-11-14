71083a79ca20035c1a634890ee5591b0b8f1aa0d
Artashes <developer.artashes@gmail.com>
2024-11-14 04:19:22 -0800

    fixes

--- packages/configs/axios.config.ts
+++ packages/configs/axios.config.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const token = localStorage.getItem("token");
+axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL_DEV;
 axios.defaults.headers.common = {
   ["Authorization"]: token ? `Bearer ${token}` : undefined,
   "X-localization": "en",
