3542ab3742264a16363f8f8e82d9d8396368dfa9
Artashes <developer.artashes@gmail.com>
2024-11-14 04:14:48 -0800

    fix

--- apps/ge/src/main.tsx
+++ apps/ge/src/main.tsx
@@ -6,6 +6,8 @@ import "@repo/configs/axios.config";
 import "@repo/ui/assets/global.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./utils/hooks/useAuth";
+import axios from "axios";
+axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL_DEV;
 
 const root = document.getElementById("root-ge") as HTMLDivElement;
 ReactDOM.createRoot(root).render(
